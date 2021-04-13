5e7358c10c11c43617472f111cb9fa7b2024caa3
ngarawakimani <dancankimani70@gmail.com>
2021-04-14 00:42:56 +0300

    Added Swagger docs

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-// import { BootstrapConsole } from 'nestjs-console';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  // app.use(BootstrapConsole);
+  const appOptions = { cors: true };
+  const app = await NestFactory.create(AppModule, appOptions);
+  app.setGlobalPrefix('api/v1');
+  const options = new DocumentBuilder()
+    .setTitle('Time Entries Redmine API')
+    .setDescription('A REST API to allow for time entries by users')
+    .setVersion('1.0')
+    .setBasePath('api/v1')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('/docs', app, document);
   await app.listen(3030);
 }
 bootstrap();
--- src/time-entries/time-entries.controller.ts
+++ src/time-entries/time-entries.controller.ts
@@ -3,7 +3,16 @@ import { TimeEntriesService } from './time-entries.service';
 import { HttpException } from '@nestjs/common/exceptions/http.exception';
 import { ValidationPipe } from '../shared/pipes/validation.pipe';
 import { CreateSingleUserPerProjectDto } from './dto/create.single-user-per-project.dto';
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiParam,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 
+@ApiBearerAuth()
+@ApiTags('Time Entries')
 @Controller('time-entries')
 export class TimeEntriesController {
   private projects: any;
@@ -12,20 +21,65 @@ export class TimeEntriesController {
   constructor(private readonly timeEntriesService: TimeEntriesService) {}
 
   @Get('projects')
+  @ApiOperation({ summary: 'Get list of projects' })
+  @ApiResponse({ status: 200, description: 'OK' })
+  @ApiResponse({ status: 403, description: 'Forbiden' })
   getProjects(): any {
     return this.timeEntriesService.getProjects();
   }
 
   @Get('users')
+  @ApiOperation({ summary: 'Get list of users' })
+  @ApiResponse({ status: 200, description: 'OK' })
+  @ApiResponse({ status: 403, description: 'Forbiden' })
   getUsers(): any {
     return this.timeEntriesService.getUsers();
   }
 
   @Get('issues')
+  @ApiOperation({ summary: 'Get a list of issues in all projects' })
+  @ApiResponse({ status: 200, description: 'OK' })
+  @ApiResponse({ status: 403, description: 'Forbiden' })
   getIssues(): any {
     return this.timeEntriesService.getIssues();
   }
 
+  @Post()
+  @ApiOperation({ summary: 'Create a single user per project' })
+  @ApiParam({
+    name: 'project_id',
+    type: 'number',
+    example: 1,
+  })
+  @ApiParam({
+    name: 'spent_on',
+    type: 'string',
+    example: '2021-04-13',
+  })
+  @ApiParam({
+    name: 'time_entry',
+    type: 'json',
+    example: {
+      hours: 10,
+      activity_id: 5,
+    },
+  })
+  @ApiParam({
+    name: 'comments',
+    type: 'string',
+    example: 'Lorem Ipsum is simptext ev.',
+  })
+  @ApiParam({
+    name: 'user_id',
+    type: 'number',
+    example: 1,
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'OK',
+    type: CreateSingleUserPerProjectDto,
+  })
+  @ApiResponse({ status: 422, description: 'Unprocessable Entity' })
   @UsePipes(new ValidationPipe())
   @Post('single-user-per-project')
   singleUserPerProject(
